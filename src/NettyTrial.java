99e443e12202bfd26df5d156ee30ad30ae1ae03f
ipipip1735 <ipipip1735@163.com>
2019-10-25 17:29:35 +0800

    Add Netty

--- src/NettyTrial.java
+++ src/NettyTrial.java
@@ -1,4 +1,5 @@
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -25,7 +26,7 @@ public class NettyTrial {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(group);
             serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.localAddress(new InetSocketAddress("localhost", 8080));
+            serverBootstrap.localAddress(new InetSocketAddress("192.168.0.126", 5454));
 
             ChannelInitializer<SocketChannel> init = new ChannelInitializer<>() {
                 @Override
@@ -34,37 +35,60 @@ public class NettyTrial {
                     System.out.println("ch is " + ch);
 
 
-                    ChannelHandler handler = new ChannelHandler() {
+                    ChannelInboundHandlerAdapter channelInboundHandlerAdapter = new ChannelInboundHandlerAdapter() {
                         @Override
-                        public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+                        public void handlerAdded(ChannelHandlerContext ctx) {
                             System.out.println("~~" + getClass().getSimpleName() + ".handlerAdded~~");
                             System.out.println("ctx is " + ctx);
-
                         }
 
                         @Override
-                        public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+                        public void handlerRemoved(ChannelHandlerContext ctx) {
                             System.out.println("~~" + getClass().getSimpleName() + ".handlerRemoved~~");
                             System.out.println("ctx is " + ctx);
-
                         }
 
                         @Override
-                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-                            System.out.println("~~" + getClass().getSimpleName() + ".exceptionCaught~~");
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                            System.out.println("~~" + getClass().getSimpleName() + ".channelRead~~");
                             System.out.println("ctx is " + ctx);
-                            System.out.println("cause is " + cause);
+                            System.out.println("msg is " + msg);
+                            ByteBuf byteBuf = (ByteBuf) msg;
+
+                            byte[] bytes;
+
+                            System.out.println(byteBuf.readableBytes());
+                            System.out.println("msg is " + msg);
+
+                            System.out.println(byteBuf.readByte());
+
+                            System.out.println(byteBuf.readableBytes());
+                            System.out.println(byteBuf.readByte());
+                            System.out.println(byteBuf.readableBytes());
+
+
+//                            for (int i = 0; i < bytes.length; i++) {
+//                                System.out.println(bytes[i]);
+//
+//                            }
+
+//                            System.out.println(bytes.length);
+
+
+
 
                         }
                     };
 
 
-                    ch.pipeline().addLast(handler);
+                    ch.pipeline().addLast(channelInboundHandlerAdapter);
 
                 }
             };
 
-            serverBootstrap.childHandler(init);
+            serverBootstrap.childHandler(init)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
             ChannelFuture channelFuture = serverBootstrap.bind().sync();
             channelFuture.channel().closeFuture().sync();
         } catch (Exception e) {
