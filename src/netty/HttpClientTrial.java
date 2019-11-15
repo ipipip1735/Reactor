db659b397822bfbd08fedf08cc633908d8f0420b
ipipip1735 <ipipip1735@163.com>
2019-11-15 17:30:15 +0800

    update Reactor Netty add HTTPClient

--- src/netty/HttpClientTrial.java
+++ src/netty/HttpClientTrial.java
@@ -1,5 +1,7 @@
 package netty;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.cookie.DefaultCookie;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,70 +26,238 @@ public class HttpClientTrial {
 
         HttpClientTrial httpClientTrial = new HttpClientTrial();
 
-        httpClientTrial.conn();
+//        httpClientTrial.config();
+//        httpClientTrial.connection();
+//        httpClientTrial.response();
+//        httpClientTrial.hooks();
+
+        httpClientTrial.request();
+
 
     }
 
-    private void conn() {
+    private void request() {
+
+
+        HttpClient.create()
+                .post()
+                .uri(uri)
+                .send(Mono.just(Unpooled.wrappedBuffer("OOO".getBytes())))
+                .response((httpClientResponse, byteBufFlux) -> {
+                    System.out.println(httpClientResponse.status());
+                    return byteBufFlux.asString();
+                })
+                .doOnNext(System.out::println)
+                .then()
+                .block();
+
+
+    }
+
+    private void connection() {
+
+        HttpClient.create()
+                .mapConnect((mono, bootstrap) ->
+                        mono.map(conn -> {
+                            System.out.println(conn);
+                            conn.addHandlerLast(null);
+                            System.out.println(bootstrap);
+                            return conn;
+                        })
+                )
+                .get()
+                .uri(uri)
+                .response()
+                .block();
+
+    }
+
+    private void hooks() {
+
+        //方式一
+//        HttpClient.create()
+//                .doOnRequest((httpClientRequest, connection) -> {
+//                    System.out.println("~~doOnRequest~~");
+//                    System.out.println(httpClientRequest);
+//                    System.out.println(connection);
+//                })
+//                .doAfterRequest((httpClientRequest, connection) -> {
+//                    System.out.println("~~doAfterRequest~~");
+//                    System.out.println(httpClientRequest);
+//                    System.out.println(connection);
+//                })
+//                .doOnRequestError((httpClientRequest, connection) -> {
+//                    System.out.println("~~doOnRequestError~~");
+//                    System.out.println(httpClientRequest);
+//                    System.out.println(connection);
+//                })
+//                .doOnResponse((httpClientRequest, connection) -> {
+//                    System.out.println("~~doOnResponse~~");
+//                    System.out.println(httpClientRequest);
+//                    System.out.println(connection);
+//                })
+//                .doAfterResponse((httpClientResponse, connection) -> {
+//                    System.out.println("~~doAfterResponse~~");
+//                    System.out.println(httpClientResponse);
+//                    System.out.println(connection);
+//                })
+//                .doOnError((httpClientRequest, throwable) -> {
+//                    System.out.println("~~doOnError~Reqest~");
+//                },(httpClientResponse, throwable) -> {
+//                    System.out.println("~~doOnError~Response~");
+//                })
+//                .doOnResponseError((HttpClientResponse, throwable) -> {
+//                    System.out.println("~~doOnResponseError~~");
+//                })
+//                .get()
+//                .uri(uri)
+//                .response()
+//                .doOnNext(httpClientResponse -> {
+//                    System.out.println("~~doOnNext~~");
+//                    System.out.println(httpClientResponse.status());
+//                })
+//                .then()
+//                .block();
+
+
+        //方式二：使用response()
+        HttpClient.create()
+                .doOnResponse((httpClientResponse, connection) -> {
+                    System.out.println("~~doOnResponse~~");
+                    System.out.println("connection is " + connection);
+                    System.out.println("httpClientResponse is " + httpClientResponse);
+                    System.out.println(httpClientResponse.status());
+                    System.out.println(httpClientResponse.responseHeaders());
+
+                })
+                .doAfterResponse((httpClientResponse, connection) -> {
+                    System.out.println("~~doAfterResponse~~");
+                    System.out.println("connection is " + connection);
+                    System.out.println("httpClientResponse is " + httpClientResponse);
+                    System.out.println(httpClientResponse.status());
+                    System.out.println(httpClientResponse.responseHeaders());
+
+                })
+                .get()
+                .uri(uri)
+                .response((httpClientResponse, byteBufMono) -> {
+                    System.out.println("~~response~~");
+                    System.out.println(httpClientResponse.status());
+                    System.out.println(httpClientResponse.responseHeaders());
+                    return byteBufMono.asString();
+                })
+                .doOnNext(System.out::println)
+                .then()
+                .block();
+
+        //方式三：使用responseSingle()
+//        HttpClient.create()
+//                .doOnResponse((httpClientResponse, connection) -> {
+//                    System.out.println("~~doOnResponse~~");
+//                    System.out.println("connection is " + connection);
+//                    System.out.println("httpClientResponse is " + httpClientResponse);
+//                    System.out.println(httpClientResponse.status());
+//                    System.out.println(httpClientResponse.responseHeaders());
+//
+//                })
+//                .doAfterResponse((httpClientResponse, connection) -> {
+//                    System.out.println("~~doAfterResponse~~");
+//                    System.out.println("connection is " + connection);
+//                    System.out.println("httpClientResponse is " + httpClientResponse);
+//                    System.out.println(httpClientResponse.status());
+//                    System.out.println(httpClientResponse.responseHeaders());
+//
+//                })
+//                .get()
+//                .uri(uri)
+//                .responseSingle((httpClientResponse, byteBufMono) -> {
+//                    System.out.println("~~responseSingle~~");
+//                    return byteBufMono.asString()
+//                            .doOnNext(s -> {
+//                                System.out.println(s);
+//                            })
+//                            .then();
+//                })
+//                .block();
+
+
+//        try {
+//            Thread.sleep(2000L);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+
+    private void response() {
+
+        //方式一：获取结果
+        HttpClient.create()
+                .get()
+                .uri(uri)
+                .response((httpClientResponse, byteBufMono) -> {
+                    System.out.println("~~responseSingle~~");
+                    System.out.println(httpClientResponse);
+                    System.out.println(httpClientResponse.status());
+                    System.out.println(httpClientResponse.responseHeaders());
+
+                    return byteBufMono.asString();
+                })
+                .doOnNext(System.out::println)
+                .then()
+                .block();
+
+
+        //方式二：获取单值
+        HttpClient.create()
+                .get()
+                .uri(uri)
+                .responseSingle((httpClientResponse, byteBufMono) -> {
+                    System.out.println("~~responseSingle~~");
+                    System.out.println(httpClientResponse);
+                    System.out.println(httpClientResponse.status());
+                    System.out.println(httpClientResponse.responseHeaders());
+
+                    return byteBufMono.asString();
+                })
+                .doOnNext(System.out::println)
+                .then()
+                .block();
+    }
+
+    private void config() {
 
 
         //方式一：分步使用
-        HttpClient httpClient = HttpClient.create()
-                .keepAlive(true)
-                .headers(httpHeaders -> {
-                    httpHeaders.add("xx", "y8y");
-                })
-                .compress(true);
-        HttpClient.ResponseReceiver responseReceiver = httpClient.get();
-        HttpClient.RequestSender requestSender = (HttpClient.RequestSender) responseReceiver.uri(uri);
-
-        HttpClientResponse httpClientResponse = requestSender.response()
-                .block();
-        System.out.println("status is " + httpClientResponse.status());
-
-
-//        httpClient.baseUrl();
-//        HttpClient.RequestSender requestSender = (HttpClient.RequestSender) httpClient.get();
+//        HttpClient httpClient = HttpClient.create()
+//                .keepAlive(true)//启用长连接
+//                .headers(httpHeaders -> {
+//                    httpHeaders.add("xx", "y8y");
+//                })
+//                .compress(true);
+//        HttpClient.ResponseReceiver responseReceiver = httpClient.get();
+//        HttpClient.RequestSender requestSender = (HttpClient.RequestSender) responseReceiver.uri(uri);
+//
+//        HttpClientResponse httpClientResponse = requestSender.response()
+//                .block();
+//        System.out.println("status is " + httpClientResponse.status());
 
 
         //方式二：使用链式写法
-//        HttpClientResponse response =
-//                HttpClient.create()
-//                        .get()
-//                        .uri(uri)
-//                        .response()
-//                        .block();
+        HttpClientResponse response =
+                HttpClient.create()
+                        .keepAlive(true)//启用长连接
+                        .headers(httpHeaders -> {
+                            httpHeaders.add("xx", "y8y");
+                        })
+                        .compress(true)
+                        .get()
+                        .uri(uri)
+                        .response()
+                        .block();
 
-
-        //方式三：使用链式写法
-//        HttpClient.create()
-//                .get()
-//                .uri(uri)
-//                .response((httpClientResponse, byteBufFlux) -> {
-//                    System.out.println("~~response~~");
-//                    System.out.println("status is " + httpClientResponse.status());
-//                    System.out.println("responseHeaders is " + httpClientResponse.responseHeaders());
-//                    System.out.println("currentContext is " + httpClientResponse.currentContext());
-//                    System.out.println("cookies is " + httpClientResponse.cookies());
-//
-//
-////                    byteBufFlux.asString()
-////                            .doOnNext(byteBuf -> {
-////                        System.out.println("~~doOnNext~~");
-////                        System.out.println(byteBufFlux);
-////                    }).subscribe();
-//
-//
-//
-//                    return Flux.just("oo");
-//                })
-//                .blockLast();
-
-
-        try {
-            Thread.sleep(6000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println(response.responseHeaders());
+        System.out.println(response.status());
     }
 }
--- src/netty/HttpServerTrial.java
+++ src/netty/HttpServerTrial.java
@@ -46,12 +46,13 @@ public class HttpServerTrial {
 
     private void request() {
 
+        //POST请求
         HttpServer.create()
 //                .host(host)
                 .host(ip)
                 .port(port)
                 .route(routes ->
-                        routes.get("/test/{param}", (request, response) -> {
+                        routes.post("/test/ddd", (request, response) -> {
 
                                     System.out.println("hostAddress is " + request.hostAddress());
                                     System.out.println("scheme is " + request.scheme());
@@ -81,6 +82,7 @@ public class HttpServerTrial {
 
                                     request.withConnection(connection -> {
                                         System.out.println("connection is " + connection);
+                                        connection.addHandlerLast(null);
                                     });
 
 
@@ -92,19 +94,79 @@ public class HttpServerTrial {
                                                 System.out.println(byteBuf);
                                             });
 
-                                    request.receiveObject()
-                                            .doOnNext(byteBuf -> {
-                                                System.out.println("~~doOnNext~~");
-                                                System.out.println(byteBuf);
-                                            });
-
-
-
-                                    return response.sendString(Mono.just("ok")).then();
+//                                    request.receiveObject()
+//                                            .doOnNext(byteBuf -> {
+//                                                System.out.println("~~doOnNext~~");
+//                                                System.out.println(byteBuf);
+//                                            });
+                                    return response.sendString(Mono.just("post ok")).then();
                                 }
                         ))
                 .bindNow()
                 .onDispose()
                 .block();
+
+
+        //GET请求
+//        HttpServer.create()
+////                .host(host)
+//                .host(ip)
+//                .port(port)
+//                .route(routes ->
+//                        routes.get("/test/{param}", (request, response) -> {
+//
+//                                    System.out.println("hostAddress is " + request.hostAddress());
+//                                    System.out.println("scheme is " + request.scheme());
+//                                    System.out.println("param is " + request.param("param"));
+//                                    System.out.println("remoteAddress is " + request.remoteAddress());
+//                                    System.out.println("requestHeaders is " + request.requestHeaders());
+//                                    System.out.println(request.params());
+//
+//                                    System.out.println("cookies is " + request.cookies());
+//                                    System.out.println("isKeepAlive is " + request.isKeepAlive());
+//                                    System.out.println("isWebsocket is " + request.isWebsocket());
+//                                    System.out.println("method is " + request.method());
+//                                    System.out.println("path is " + request.path());
+//                                    System.out.println("uri is " + request.uri());
+//                                    System.out.println("version is " + request.version());
+//
+//                                    System.out.println("-------------");
+//
+//                                    request.paramsResolver(s -> {
+//                                        System.out.println("paramsResolver is " + s);
+//                                        return new HashMap<>();
+//                                    });
+//
+//                                    request.receiveContent().doOnNext(httpContent -> {
+//                                        System.out.println("receiveContent is " + httpContent);
+//                                    });
+//
+//                                    request.withConnection(connection -> {
+//                                        System.out.println("connection is " + connection);
+//                                    });
+//
+//
+//                                    System.out.println("-------------");
+//
+//                                    request.receive()
+//                                            .doOnNext(byteBuf -> {
+//                                                System.out.println("~~doOnNext~~");
+//                                                System.out.println(byteBuf);
+//                                            });
+//
+//                                    request.receiveObject()
+//                                            .doOnNext(byteBuf -> {
+//                                                System.out.println("~~doOnNext~~");
+//                                                System.out.println(byteBuf);
+//                                            });
+//
+//
+//
+//                                    return response.sendString(Mono.just("ok")).then();
+//                                }
+//                        ))
+//                .bindNow()
+//                .onDispose()
+//                .block();
     }
 }
